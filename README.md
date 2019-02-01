ef6454a9cbe35e943c2250c395e4dadad85198e2
And-Rasta <avangaea@gmail.com>
2019-02-01 15:37:27 -0600

    Index and views

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Burger
 Assignment # 13: Burger 2: The Sequel
+
+Express-Handlebars and MySQL.
---
+++ public/js/burgers.js
@@ -0,0 +1,17 @@
+// JS tied to Index
+$(function() {
+
+    $('.devour-button').on('click', devourClick);
+
+    function devourClick() {
+        let id = $(this).attr('data-id');
+        let url = '/api/update/' + id;
+        $.ajax({
+            url: url,
+            method: "POST",
+            success: function() {
+                location.reload();
+            }
+        })
+    }
+});
