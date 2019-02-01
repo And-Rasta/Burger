a2526cd42fd12ed7cb7909754c1b54e5e597e06f
And-Rasta <avangaea@gmail.com>
2019-02-01 15:13:58 -0600

    Adding db files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Mushroom Swiss', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Feta Cranberry Goat', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Bison Scotch Bonnet', true);
